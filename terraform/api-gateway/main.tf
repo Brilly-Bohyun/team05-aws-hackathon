a9de450d6d12a46c57c1f615cfc7eccc2d7d950b
binhao <binhao@binhaoui-MacBookAir.local>
2025-09-06 02:19:06 +0900

    slack to apigateway URL edit

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -33,10 +33,56 @@ resource "aws_lambda_permission" "api_gateway_invoke" {
   source_arn    = "${aws_api_gateway_rest_api.terraform_sync.execution_arn}/*/*"
 }
 
+resource "aws_api_gateway_method" "slack_actions_options" {
+  rest_api_id   = aws_api_gateway_rest_api.terraform_sync.id
+  resource_id   = aws_api_gateway_resource.slack_actions.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "slack_actions_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.terraform_sync.id
+  resource_id = aws_api_gateway_resource.slack_actions.id
+  http_method = aws_api_gateway_method.slack_actions_options.http_method
+  type        = "MOCK"
+  
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "slack_actions_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.terraform_sync.id
+  resource_id = aws_api_gateway_resource.slack_actions.id
+  http_method = aws_api_gateway_method.slack_actions_options.http_method
+  status_code = "200"
+  
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "slack_actions_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.terraform_sync.id
+  resource_id = aws_api_gateway_resource.slack_actions.id
+  http_method = aws_api_gateway_method.slack_actions_options.http_method
+  status_code = aws_api_gateway_method_response.slack_actions_options_response.status_code
+  
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 resource "aws_api_gateway_deployment" "terraform_sync" {
   depends_on = [
     aws_api_gateway_method.slack_actions_post,
-    aws_api_gateway_integration.slack_actions_integration
+    aws_api_gateway_integration.slack_actions_integration,
+    aws_api_gateway_method.slack_actions_options,
+    aws_api_gateway_integration.slack_actions_options_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.terraform_sync.id
