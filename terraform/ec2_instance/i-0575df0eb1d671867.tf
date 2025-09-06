ddefb9201c36659971fa7c4e5fe812a4677beb2e
binhao <binhao@binhaoui-MacBookAir.local>
2025-09-06 13:58:16 +0900

    테스트 리소스 제거

--- terraform/ec2_instance/i-0575df0eb1d671867.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "i-0575df0eb1d671867" {
-  ami           = "ami-06883c95d78ea3e7a"
-  instance_type = "t3.micro"
-  subnet_id     = "subnet-0f0a7f72b7f1d65af"
-  vpc_security_group_ids = ["sg-0a028877b6e96c0bc"]
-}
--- terraform/s3_bucket/test-rocket-19.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "test-rocket-19" {
-  bucket = "test-rocket-19"
-}
