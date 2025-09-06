9bb6b26df7ad02a5a12911759a5bb7dafd1aaa3d
binhao <binhao@binhaoui-MacBookAir.local>
2025-09-06 13:21:19 +0900

    테스트 리소스 제거

--- terraform/ec2_instance/i-035381c1d7ce2d746.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "i-035381c1d7ce2d746" {
-  ami           = "ami-00ca32bbc84273381"
-  instance_type = "t3.micro"
-  subnet_id     = "subnet-0cd8237100f6181a4"
-  vpc_security_group_ids = ["sg-0fc0b863835b5d766"]
-}
--- terraform/ec2_instance/i-0c820c310c13f95c8.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "i-0c820c310c13f95c8" {
-  # Import existing instance: i-0c820c310c13f95c8
-  # Run: terraform import aws_instance.i-0c820c310c13f95c8 i-0c820c310c13f95c8
-}
